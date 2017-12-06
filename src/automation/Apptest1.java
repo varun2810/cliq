6558e7fd419654374f8aa5e650fb1093de944ca0
varun2810 <varunvishal28@gmail.com>
2017-12-06 17:35:31 +0530

    Initial commit

---
+++ src/automation/Apptest1.java
@@ -0,0 +1,74 @@
+package automation;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Apptest1 {
+	
+	static XSSFWorkbook workbook;
+    static XSSFSheet sheet;
+	static XSSFCell cell;
+	static String URL = "http://www.google.com";
+	static WebDriver driver;
+	
+	static File src=new File(".\\testdata\\Automation.xlsx");
+	
+	static methods obj = new methods();
+	
+	
+	
+	 @Test
+	  public static void excel() throws IOException, InterruptedException 
+		{ 
+				
+		 	// Load the file.
+			FileInputStream finput = new FileInputStream(src);
+			
+			// Load the workbook.
+			workbook = new XSSFWorkbook(finput);
+			
+			// Load the sheet in which data is stored.
+			sheet= workbook.getSheetAt(0);
+			
+			int rc=sheet.getLastRowNum();
+			
+			for(int i=1; i<=rc; i++)							
+				
+				{					
+					 Row row = sheet.getRow(i);	
+					 
+					 if(row == null || row.getCell(1)==null) {					 
+						 continue;
+				}
+					 				 
+					 String testcase = row.getCell(1).toString();									
+					 System.out.println(testcase);
+					 
+					 switch(testcase) {
+					 
+					 case "App installation":
+							System.out.println("testcase : " + testcase);									
+							obj.installation();					
+							Thread.sleep(3000);
+							//updateresult("//*[@id='lst-ib']",i);
+							break;
+					 
+					 }
+						 
+						 
+					 
+										
+						
+						
+				     }
+		}
+	 
+}
---
+++ src/automation/methods.java
@@ -0,0 +1,43 @@
+package automation;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class methods {
+	
+	@SuppressWarnings("rawtypes")
+	public static AndroidDriver driver;
+	static String email = "varunvishal08@gmail.com";
+	static String password = "Tour2810";
+  
+	
+	@Test
+	public void installation() throws MalformedURLException {
+		
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("deviceName", "350df4d27d42");
+
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
+		//capabilities.setCapability(CapabilityType.VERSION, "7.1.2");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("app", "C:\\Users\\Varun\\workspace\\Cliq\\apks\\Cliq.apk");
+		capabilities.setCapability("appPackage", "com.zoho.chat");
+		capabilities.setCapability("appActivity", "com.zoho.chat.ui.MyBaseActivity");
+		capabilities.setCapability("noReset", false);
+
+
+		driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+			
+		
+  }
+}
